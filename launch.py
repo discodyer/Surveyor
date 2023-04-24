ec81d479c604c22c9170a98c83a671d01c7d694c
discodyer <cody23333@gmail.com>
2023-04-25 00:43:46 +0800

    起飞测试

--- launch.py
+++ launch.py
@@ -13,7 +13,7 @@ from dronekit import connect, VehicleMode, LocationGlobalRelative
 
 
 # 通过本地的14551端口，使用UDP连接到SITL模拟器
-connection_string = 'com10'
+connection_string = '/dev/ttyS3'
 print('Connecting to vehicle on: %s' % connection_string)
 # connect函数将会返回一个Vehicle类型的对象，即此处的vehicle
 # 即可认为是无人机的主体，通过vehicle对象，我们可以直接控制无人机
@@ -32,8 +32,8 @@ def arm_and_takeoff(aTargetAltitude):
 
     # 解锁无人机（电机将开始旋转）
     print("Arming motors")
-    # 将无人机的飞行模式切换成"GUIDED_NOGPS"（一般建议在GUIDED模式下控制无人机）
-    vehicle.mode = VehicleMode("GUIDED_NOGPS")
+    # 将无人机的飞行模式切换成"GUIDED"（一般建议在GUIDED模式下控制无人机）
+    vehicle.mode = VehicleMode("GUIDED")
     # 通过设置vehicle.armed状态变量为True，解锁无人机
     vehicle.armed = True
 
@@ -62,32 +62,32 @@ def arm_and_takeoff(aTargetAltitude):
 arm_and_takeoff(1)
 
 # 设置在运动时，默认的空速为3m/s
-print("Set default/target airspeed to 3")
+# print("Set default/target airspeed to 3")
 # vehicle.airspeed变量可读可写，且读、写时的含义不同。
 # 读取时，为无人机的当前空速；写入时，设定无人机在执行航点任务时的默认速度
-vehicle.airspeed = 3
+# vehicle.airspeed = 3
 
 # 发送指令，让无人机前往第一个航点
-print("Going towards first point for 30 seconds ...")
+# print("Going towards first point for 30 seconds ...")
 # LocationGlobalRelative是一个类，它由经纬度(WGS84)和相对于home点的高度组成
 # 这条语句将创建一个位于南纬35.361354，东经149.165218，相对home点高20m的位置
-point1 = LocationGlobalRelative(-35.361354, 149.165218, 20)
+# point1 = LocationGlobalRelative(-35.361354, 149.165218, 20)
 # simple_goto函数将位置发送给无人机，生成一个目标航点
-vehicle.simple_goto(point1)
+# vehicle.simple_goto(point1)
 
 # simple_goto函数只发送指令，不判断有没有到达目标航点
 # 它可以被其他后续指令打断，此处延时30s，即让无人机朝向point1飞行30s
-time.sleep(30)
+# time.sleep(30)
 
 # 发送指令，让无人机前往第二个航点
-print("Going towards second point for 30 seconds (groundspeed set to 10 m/s) ...")
+# print("Going towards second point for 30 seconds (groundspeed set to 10 m/s) ...")
 # 与之前类似，这条语句创建了另一个相对home高20m的点
-point2 = LocationGlobalRelative(-35.363244, 149.168801, 20)
+# point2 = LocationGlobalRelative(-35.363244, 149.168801, 20)
 # simple_goto将目标航点发送给无人机，groundspeed=10设置飞行时的地速为10m/s
-vehicle.simple_goto(point2, groundspeed=10)
+# vehicle.simple_goto(point2, groundspeed=10)
 
 # 与之前一样，延时30s
-time.sleep(30)
+time.sleep(10)
 
 # 发送"返航"指令
 print("Returning to Launch")
